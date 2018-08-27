cc730d2c32726e7a830777914b74e44eba64fe87
Uwe van Heesch <uwe@vanheesch.net>
2018-08-27 15:56:47 +0200

    Generate HTTP header based on filecontent and current date/time.

--- src/main/java/nl/han/dea/http/ConnectionHandler.java
+++ src/main/java/nl/han/dea/http/ConnectionHandler.java
@@ -5,14 +5,21 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ConnectionHandler implements Runnable {
 
-    private static final String HTTP_HEADER = "HTTP/1.1 200 OK\n" +
-            "Date: Mon, 27 Aug 2018 14:08:55 +0200\n" +
-            "HttpServer: Simple DEA Webserver\n" +
-            "Content-Length: 90\n" +
+    private static final String SERVER_NAME = "Simple DEA Webserver";
+    private static final String HTTP_STATUS_200 = "200 OK";
+    private static final String INDEX_HTML_PAGE = "pages/index.html";
+
+    private final String HEADER_TEMPLATE = "HTTP/1.1 {{HTTP_STATUS}}\n" +
+            "Date: {{DATE}}\n" +
+            "HttpServer: " + SERVER_NAME + "\n" +
+            "Content-Length: {{CONTENT_LENGTH}}\n" +
             "Content-Type: text/html\n";
+
 
     private Socket socket;
 
@@ -42,20 +49,17 @@ public class ConnectionHandler implements Runnable {
     }
 
     private void writeResponseMessage(BufferedWriter outputStreamWriter) {
+
         try {
-            outputStreamWriter.write(HTTP_HEADER);
+            outputStreamWriter.write(generateHeader(HTTP_STATUS_200, INDEX_HTML_PAGE));
             outputStreamWriter.newLine();
-            outputStreamWriter.write(readFile("pages/index.html"));
+            outputStreamWriter.write(readFile(INDEX_HTML_PAGE));
             outputStreamWriter.newLine();
             outputStreamWriter.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-    }
-
-    private boolean lineMarksEndOfRequest(String line) {
-        return line.isEmpty();
     }
 
     private String readFile(String filename) {
@@ -66,9 +70,27 @@ public class ConnectionHandler implements Runnable {
         }
     }
 
+    private String generateHeader(String status, String filename) {
+        String header = HEADER_TEMPLATE
+                .replace("{{DATE}}",
+                        OffsetDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME))
+                .replace("{{HTTP_STATUS}}", status);
+
+        String contentLength = Long.toString(0);
+        if (filename != null) {
+            contentLength = Long.toString(getPath(filename).toFile().length());
+        }
+        header = header.replace("{{CONTENT_LENGTH}}", contentLength);
+        return header;
+    }
+
     private Path getPath(String filename) {
         ClassLoader classLoader = getClass().getClassLoader();
         return new File(classLoader.getResource(filename).getFile()).toPath();
+    }
+
+    private boolean lineMarksEndOfRequest(String line) {
+        return line.isEmpty();
     }
 
 }
